66571b31ceee6c3d9c041aac95ace59b0a41f170
alunos <alunos@etehort.ps.sp>
2022-02-23 19:24:08 -0300

    Kevin - Inicial

---
+++ SistemaAcademico/Model/pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAcademico.Model
+{
+    class pessoa
+    {
+        public long cpf;
+        public string nome;
+        public DateTime dataNascimento;
+    }
+}
---
+++ SistemaAcademico/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SistemaAcademico.Model;
+
+namespace SistemaAcademico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            aluno objaluno = new aluno(); //aluno () Método construtor
+            professor joao = new professor();
+            int x;
+            objaluno.cpf = 12344387089.ToString();
+            objaluno.dataNascimento = new DateTime(2004, 11, 27);     
+            joao.cpf = 32423412.ToString();
+            objaluno.nome = "Maria";
+            joao.nome = "João Pedro";
+
+            Console.WriteLine("CPF:");
+            Console.WriteLine(objaluno.cpf);
+            Console.WriteLine();
+            Console.WriteLine("Nome:");
+            Console.WriteLine(objaluno.nome);
+            Console.WriteLine();
+            Console.WriteLine("Data de Nascimento:");
+            Console.WriteLine(objaluno.dataNascimento.ToString("dd/MM/yyyy"));
+            Console.ReadKey();                     
+        }
+    }
+}
